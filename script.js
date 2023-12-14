b4cfcaaf3d6ac519e3fbbbdac91aa16b1d3a39f9
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-12-14 18:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let boxes= document.querySelectorAll(".square");
+// console.log(boxes);
+
+boxes.forEach(box=>{
+	box.addEventListener("mouseenter", while_entering);
+	box.addEventListener("mouseleave", while_leaving);
+});
+
+function while_entering(event) {
+	// console.log(event.target);
+	boxes.forEach(box=>{
+		if(box.id!==event.target.id){
+			box.classList.add("active");
+		}
+	})
+	// console.log("hovering");
+}
+
+function while_leaving() {
+	boxes.forEach(box=>{
+		if(box.classList.contains("active")){
+			box.classList.remove("active");
+		}
+	})
+	
+}
